05de11994dd5cc570be6c6ba915f6ba3c29bfbd2
dioneuty <1@293f3900>
2024-01-11 21:56:56 +0900

    enter 컴포넌트 스타일 넣어주기

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 
+function cls(...classnames:string[]){
+    return classnames.join(' ');
+}
+
 export default function Enter() {
     const [method, setMethod] = useState<"email" | "phone">("email");
     const onEmailClick = () => setMethod("email");
     const onPhoneClick = () => setMethod("phone");
     return (
-        <div>
-            <h3>Enter to Carrot</h3>
-            <div>
-                <div>
-                    <h5>Enter using:</h5>
-                    <div>
-                        <button onClick={onEmailClick}>Email</button>
-                        <button onClick={onPhoneClick}>Phone</button>
+        <div className={`mt-16`}>
+            <h3 className={`text-3xl font-bold text-center`}>Enter to Carrot</h3>
+            <div className={`mt-8`}>
+                <div className={`flex flex-col items-center`}>
+                    <h5 className={`text-sm text-gray-500 font-medium`}>Enter using:</h5>
+                    <div className={`grid border-b mt-8 w-full grid-cols-2 gap-16`}>
+                        <button className={cls("pb-4 font-medium border-b-2", method === "email" ? "border-orange-500 text-orange-400" : "border-transparent text-gray-500")} onClick={onEmailClick}>Email</button>
+                        <button className={cls("pb-4 font-medium border-b-2", method === "phone" ? "border-orange-500 text-orange-400" : "border-transparent text-gray-500")} onClick={onPhoneClick}>Phone</button>
                     </div>
                 </div>
                 <form>
