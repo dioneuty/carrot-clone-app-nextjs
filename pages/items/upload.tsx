87cb7acf10eece709dc3960c3f3d0b6473a53b1e
dioneuty <1@293f3900>
2024-01-15 07:08:25 +0900

    제품 등록 화면 name 필드 생성

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -4,7 +4,8 @@ const Upload: NextPage = () => {
     return (
         <div className={`px-4 py-16`}>
             <div>
-                <label className={`w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md`}>
+                <label
+                    className={`w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md`}>
                     <svg
                         className="h-12 w-12"
                         stroke="currentColor"
@@ -20,8 +21,20 @@ const Upload: NextPage = () => {
                         />
                     </svg>
 
-                    <input className={`hidden`} type="file" />
+                    <input className={`hidden`} type="file"/>
                 </label>
+            </div>
+            <div className={`my-5`}>
+                <label
+                    className={`mb-1 block text-sm font-medium text-gray-700`}
+                    htmlFor={`name`}
+                >Name
+                </label>
+                <div className={`rounded-md flex items-center shadow-sm`}>
+                    <input id={`name`}
+                           className='appearance-none w-full px-3 py-2 border-gray-300 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
+                           type="text" />
+                </div>
             </div>
             <div className={`my-5`}>
                 <label
@@ -33,7 +46,9 @@ const Upload: NextPage = () => {
                     <div className={`absolute left-0 pointer-events-none pl-3 flex items-center justify-center`}>
                         <span className={`text-gray-500 text-sm`}>$</span>
                     </div>
-                    <input id={`price`} className='appearance-none pl-7 w-full px-3 py-2 border-gray-300 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500' type="text" placeholder="0.00" />
+                    <input id={`price`}
+                           className='appearance-none pl-7 w-full px-3 py-2 border-gray-300 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
+                           type="text" placeholder="0.00"/>
                     <div className={`absolute right-0 pointer-events-none pr-3 flex items-center`}>
                         <span className={`text-gray-500`}>USD</span>
                     </div>
@@ -41,7 +56,9 @@ const Upload: NextPage = () => {
             </div>
             <div>
                 <label className={`mb-1 block text-sm font-medium text-gray-700`}>Description</label>
-                <textarea className={`mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500`} rows={4} />
+                <textarea
+                    className={`mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500`}
+                    rows={4}/>
             </div>
             <button
                 className='mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:offset-2 focus:ring-orange-500 focus:outline-none'
