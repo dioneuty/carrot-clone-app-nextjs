022a55e876c3ae8b5be1bb0b3d8a1aa34996d5f2
dioneuty <1@293f3900>
2024-01-28 11:10:02 +0900

    prisma client 연동 후 api route 추가

--- libs/client.ts
+++ libs/client.ts
@@ -1,9 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
 
-const client = new PrismaClient();
-
-client.user.create({data: {
-    email: "hi",
-    name: "hi",
-}})
+export default new PrismaClient();
