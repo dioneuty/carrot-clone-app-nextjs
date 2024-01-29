d4aca55a5f15c573c5b1de31abc445f759f77157
dioneuty <1@293f3900>
2024-01-30 01:13:00 +0900

    react-hook-form : email error style
    useform checking time setting

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -13,15 +13,18 @@ interface LoginForm {
   username: string;
   email: string;
   password: string;
+  errors?: string;
 }
 
 export default function Forms() {
 
   const {
     register,
-    watch,
+    //watch,
     handleSubmit,
     formState: {errors},
+    setValue,
+    setError,
   } = useForm<LoginForm>({
     mode: "onChange",
   });
@@ -30,13 +33,17 @@ export default function Forms() {
 
   const onValid = (data: LoginForm) => {
     console.log('valid');
+    setError('errors', {message:'backend is error'});
   }
 
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   }
 
-  console.log(errors);
+  //setValue("username", 'efssefe')
+
+  //console.log(errors);
+  //setValue('username', 'hello');
 
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
@@ -68,6 +75,7 @@ export default function Forms() {
         type="password" placeholder='Password'
       />
       <input type="submit" value="Create Account" />
+      {errors.errors?.message}
     </form>
   );
 }
