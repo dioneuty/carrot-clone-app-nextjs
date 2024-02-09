d21c9700b18e17eee19ba97940d750fc15f85e7b
dioneuty <1@293f3900>
2024-02-10 05:35:25 +0900

    경로 간소화 수정

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,6 +1,6 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import withHandler from "../../../libs/server/withHandler";
-//import client from '../../../libs/server/client'
+import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function handler(req:NextApiRequest, res:NextApiResponse){
     /*await client.user.create({data: {
